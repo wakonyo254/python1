adbf290aae57de38a8921541a183eb255e47bc5f
wakonyo254 <wakonyomacharia003@gmail.com>
2024-11-01 22:21:31 +0300

    Week 1 python assignment completed

---
+++ week1py/user_input.py
@@ -0,0 +1,7 @@
+name = input("Enter your name:")
+age = input("Enter your age:")
+location = input("Enter your current location:")
+
+
+
+print(f"My name is {name}, {age} yrs, currently living in {location}!")
